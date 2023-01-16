fce38bf57104f73f4be14fb0c4ed3e2851e83d59
Yobi2019 <553882618@qq.com>
2023-01-16 22:12:31 +0800

    增加右键菜单->‘启动时自动加载上一次编辑的笔记’

--- YobiDesktopNote.py
+++ YobiDesktopNote.py
@@ -1,22 +1,26 @@
-from tkinter import Tk, Label, Button, Scale, X, Menu, HORIZONTAL, BOTTOM, TOP, \
-    Text, NONE, Scrollbar, RIGHT, filedialog, dialog, messagebox
+from tkinter import Tk, Label, Scale, X, Menu, HORIZONTAL, BOTTOM, TOP, filedialog, messagebox, IntVar
 from tkinter.scrolledtext import ScrolledText
 import json
+import os
 
 user_cfg_fp = './user_cfg.json'
-win_alpha_cfg = 0
+tmp_file_path = './tmp_file.txt'
+win_alpha_cfg = 80
+auto_load_tmp_file_cfg = False
+tmp_file_content = None
 
+# 载入用户配置文件
 try:
     f = open(user_cfg_fp, 'r')
     content = json.load(f)
     win_alpha_cfg = content.get('win_alpha')
+    auto_load_tmp_file_cfg = content.get('auto_load_tmp_file')
     f.close()
 except Exception as e:
     print(e)
-    init_cfg = json.dumps({'win_alpha': '80'})
+    init_cfg = json.dumps({'win_alpha': '80', 'auto_load_tmp_file': False})
     with open(user_cfg_fp, 'w') as init_f:
         init_f.write(init_cfg)
-    win_alpha_cfg = 80
 
 
 class YobiDesktopNote:
@@ -61,6 +65,10 @@ class YobiDesktopNote:
         self.func_label.pack(fill=X)
         # 创建右键菜单
         self.right_menu = Menu(self.func_label, tearoff=False)
+        self.right_menu_auto_load_tmp_var = IntVar()
+        self.right_menu_auto_load_tmp_var.set(1 if auto_load_tmp_file_cfg else 0)
+        self.right_menu.add_checkbutton(label='启动时自动加载上一次编辑的笔记', onvalue=1, offvalue=0,
+                                        variable=self.right_menu_auto_load_tmp_var)
         self.right_menu.add_command(label='保存笔记为文本文件', command=self.save_note_text)
         self.right_menu.add_command(label='置顶/取消置顶', command=self.win_set_topmost)
         self.right_menu.add_command(label="关闭", command=self.on_close)
@@ -70,29 +78,33 @@ class YobiDesktopNote:
         self.func_label.bind("<ButtonPress-1>", self.move_start)
         self.func_label.bind("<B1-Motion>", self.move)
         self.func_label.bind("<ButtonRelease-1>", self.move_end)
-        # 显示有关信息
-        # self.info_label = Label(self.root, text='')
-        # self.info_label.pack(fill=X)
         # 笔记编辑区域
         self.notes_area = ScrolledText(self.root)
-        # 滚动条
-        # self.h_scrollbar = Scrollbar(self.root)
-        # self.h_scrollbar.pack(side=RIGHT, fill='y')
-        # self.notes_area = Text(self.root, yscrollcommand=self.h_scrollbar.set)
         self.notes_area.pack(fill=X, expand=True, side=TOP)
         # 窗体透明度条
         self.win_transparent_scale = Scale(self.root, from_=100.0, to=20.0, resolution=1,
                                            sliderlength=20, orient=HORIZONTAL, command=self.set_win_alpha)
         self.win_transparent_scale.set(self.win_alpha_cfg)
         self.win_transparent_scale.pack(fill=X, side=BOTTOM)
-        # 占位
-        # Label(self.root, text='').pack(fill=X)
-        # 关闭按钮
-        # self.quit_btn = Button(self.root, text='退出', command=self.on_close)
-        # self.quit_btn.pack(fill=X, side=BOTTOM)
 
         self.win_set_color()
+        self.load_tmp_file()
         self.root.mainloop()
+
+    def load_tmp_file(self):
+        if not auto_load_tmp_file_cfg:
+            return
+
+        global tmp_file_content
+        # 载入临时笔记文件
+        if not os.path.exists(tmp_file_path):
+            return
+        with open(tmp_file_path, 'r', encoding='utf-8') as tmp_f:
+            tmp_file_content = tmp_f.read()
+        if not tmp_file_content:
+            return
+
+        self.notes_area.insert('end', tmp_file_content)
 
     def win_set_color(self):
         self.func_label.configure(bg=self.main_color)
@@ -101,14 +113,22 @@ class YobiDesktopNote:
         self.notes_area.configure(bg=self.notes_bg_color)
 
     def on_close(self):
+        # 保存用户配置
         with open(user_cfg_fp, 'r+') as fp:
             data = json.load(fp)
             data['win_alpha'] = self.win_transparent_scale.get()
+            data['auto_load_tmp_file'] = True if self.right_menu_auto_load_tmp_var.get() == 1 else False
             # 文件指针移动到文件头
             fp.seek(0)
             # 清空文件内容
             fp.truncate()
             fp.write(json.dumps(data))
+
+        # 保存笔记内容到临时文件
+        note_content = self.notes_area.get('1.0', 'end-1c')
+        if note_content.strip():
+            with open(tmp_file_path, 'w', encoding='utf-8') as tmp_fp:
+                tmp_fp.write(note_content)
 
         self.root.quit()
         self.root.destroy()
@@ -122,9 +142,9 @@ class YobiDesktopNote:
             return
         with open(save_path, 'w', encoding='utf-8') as save_fp:
             save_fp.write(self.notes_area.get('1.0', 'end-1c'))
-        # dialog.Dialog(None, {'title': '保存完成', 'text': '文件已保存至' + save_path,
-        #                      'bitmap': '', 'strings': ('确定', ),
-        #                      'default': ''})
+        # Dialog(None, {'title': '保存完成', 'text': '文件已保存至' + save_path,
+        #               'bitmap': '', 'strings': ('确定',),
+        #               'default': ''})
         messagebox.showinfo('保存成功！', '已保存至' + save_path)
 
     def win_set_topmost(self):
@@ -139,7 +159,10 @@ class YobiDesktopNote:
 
     # 右键菜单弹出方式
     def popup(self, event):
-        self.right_menu.post(event.x_root, event.y_root)
+        try:
+            self.right_menu.post(event.x_root, event.y_root)
+        finally:
+            self.right_menu.grab_release()
 
     # 鼠标左键移动：拖动窗口
     def move(self, event):
